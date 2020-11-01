f38f3d0e22ddbc0ea96bc62a831ac56f245721a9
Shyaboi <ian.shane.sears@gmail.com>
2020-11-01 16:59:23 -0700

    added movement cooldown and cooldown UI display

--- public/sketch.js
+++ public/sketch.js
@@ -7,37 +7,81 @@ let rot = Math.floor(Math.random(1)*255)
 let gran = Math.floor(Math.random(1)*255)
 let blau = Math.floor(Math.random(1)*255)
 
+let wOnCoolDown = false
+let aOnCoolDown = false
+let sOnCoolDown = false
+let dOnCoolDown = false
+
+
 
 window.addEventListener("keydown", function (e) {
+  let ogCol = color(86, 163, 47);
+  let coolDownCol = color(255, 85, 0);
   if (e.defaultPrevented) {
     return; // Do nothing if the event was already processed
   }
   let keyDown = e.key;
-  if (keyDown === "a") {
-    x = x - 10;
-    let userPos = { id: socket.id, pos:{x:-10,y:0}};
-    socket.emit("userMove", userPos);
-  }
-
-  if (keyDown === "d") {
-    x = x + 10;
-    let userPos = { id: socket.id, pos:{x:10,y:0} };
-    socket.emit("userMove", userPos);
-  }
-
+  
   if (keyDown === "w") {
-    y = y - 10;
-    let userPos = { id: socket.id, pos:{x:0,y:-10} };
+    if (wOnCoolDown){return}
+    setTimeout(() => {
+      wOnCoolDown=false
+      wButton.style('background-color', ogCol);
+    }, 3000);
+    console.log(wOnCoolDown)
+    wOnCoolDown = true
+    wButton.style('background-color', coolDownCol);
+
+    let userPos = { id: socket.id, pos:{x:0,y:-50} };
     socket.emit("userMove", userPos);
   }
 
+
+  if (keyDown === "a") {
+    if (aOnCoolDown){return}
+    aOnCoolDown=true
+    aButton.style('background-color', coolDownCol);
+
+    setTimeout(() => {
+      aOnCoolDown=false
+      aButton.style('background-color', ogCol);
+
+    }, 3000);
+    let userPos = { id: socket.id, pos:{x:-50,y:0}};
+    socket.emit("userMove", userPos);
+  }
+  
   if (keyDown === "s") {
-    y = y + 10;
-    let userPos = { id: socket.id, pos:{x:0,y:10} };
+    if (sOnCoolDown){return}
+    sOnCoolDown=true
+    sButton.style('background-color', coolDownCol);
+
+    setTimeout(() => {
+      sOnCoolDown=false
+      sButton.style('background-color', ogCol);
+
+    }, 3000);
+    let userPos = { id: socket.id, pos:{x:0,y:50} };
     socket.emit("userMove", userPos);
   } else {
     //   console.log('notrhin')
   }
+
+  if (keyDown === "d") {
+    if (dOnCoolDown){return}
+    dOnCoolDown=true
+    dButton.style('background-color', coolDownCol);
+
+    setTimeout(() => {
+      dOnCoolDown=false
+      dButton.style('background-color', ogCol);
+
+    }, 3000);
+    let userPos = { id: socket.id, pos:{x:50,y:0} };
+    socket.emit("userMove", userPos);
+  }
+
+
 });
 let userPool = [];
 
@@ -82,23 +126,37 @@ var randY = () => {
 };
 
 function setup() {
-  createCanvas(1080, 720);
-  // Starts in the middle
-  // x = width / 2;
-  // y = height / 2;
-  // if(userPool){
 
-  //   console.log('pools closed')
-  // }
+
+  createCanvas(1080, 720);
+  frameRate(120);
+  wButton = createButton("⇧");
+  wButton.size(75,50);
+  wButton.position(20,740);
+
+  aButton = createButton("⇦");
+  aButton.size(75,50);
+  aButton.position(100,740);
+
+  sButton = createButton("⇩");
+  sButton.size(75,50);
+  sButton.position(180,740);
+
+  dButton = createButton("⇨");
+  dButton.size(75,50);
+  dButton.position(260,740);
   setTimeout(() => {
     var newGuy = new User(socket.id, { x: randX(), y: randY() });
     // userPool.push(newGuy);
     // console.log(newUser);
     socket.emit("connected", newGuy);
-  }, 2000);
+  }, 1000);
+}
+if (wOnCoolDown) {
+  console.log('cool down')  
 }
 
-// console.log(userPool)
+
 function draw() {
   background(200);
 
